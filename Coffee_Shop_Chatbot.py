b3e824f499585860debe86aa8dc9745b53920bbe
Kervens <kdelpe@gmail.com>
2022-06-26 13:37:16 -0400

    ideas to incorporate data structures into the program

--- Coffee_Shop_Chatbot.py
+++ Coffee_Shop_Chatbot.py
@@ -1,8 +1,21 @@
 from utilities import print_message, get_size, order_latte, order_mocha
+from classes import Menu
 
 def coffee_bot():
-  print('Welcome to the Kafe Lakay! \n')
+  print('Welcome to the Kafe Lakay!')
 
+  menu_list = {
+              'Small':{'Brewed Coffee':1.50, 'Iced Coffee':1.75, 'Mocha':2.00, 'Latte':2.75},
+              'Medium':{'Brewed Coffee':2.00, 'Iced Coffee':2.25, 'Mocha':2.50, 'Latte':3.25},
+              'Large':{'Brewed Coffee':2.25, 'Iced Coffee':2.50, 'Mocha':2.75, 'Latte':3.75}
+              }
+
+  for key in menu_list:
+    print('\n' + key)
+    print('----------')
+    for value in menu_list[key]:
+        print(value, ": $", menu_list[key][value])
+  
   order_drink = 'y'
 
   drinks_list = []  
@@ -26,6 +39,8 @@ def coffee_bot():
   name = input('Can I get your name please? \n> ')
   print('Thanks, {}! Your order will be ready shortly.'.format(name))
 
+
+
 def get_drink_type():
   res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
 
@@ -39,4 +54,13 @@ def get_drink_type():
     print_message()
     return get_drink_type()
 
+def calculate_bill(self, purchased_items):
+    bill = 0
+    for purchased_item in purchased_items:
+        if purchased_item in self.items:
+            bill += self.items(purchased_item)
+    return bill
 coffee_bot()
+
+
+
--- utilities.py
+++ utilities.py
@@ -1,8 +1,8 @@
 def print_message():
   print('I\'m sorry, I did not understand your selection. Please enter the corresponding letter for your response.')
-  
+
 def get_size():
-  res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
+  res = input('\nWhat size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
   
   if res == 'a':
     return 'small'
@@ -15,7 +15,7 @@ def get_size():
     return get_size()
 
 def order_latte():
-  res = input('And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n> ')
+  res = input('What kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n> ')
 
   if res == 'a':
     return 'latte'
